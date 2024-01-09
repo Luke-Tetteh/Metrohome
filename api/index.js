adf525cb88f1fb6014f6953545d8bd6a61f697b5
Luke Tetteh <luketetteh7@gmail.com>
2024-01-09 12:46:52 +0000

    creat usermodel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import mongoose, { connect } from 'mongoose';
+import mongoose, {connect} from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';   
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=> {
@@ -8,7 +9,9 @@ mongoose.connect(process.env.MONGO).then(()=> {
 } ) 
 const app = express();
 
+app.use(express.json());
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000!!');
-}
+} 
 );
