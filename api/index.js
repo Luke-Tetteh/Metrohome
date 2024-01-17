a0a8c2718fcbc8b35d9ec204e33d2564ef2d3005
Anyikabs <114746509+Anyikabs@users.noreply.github.com>
2024-01-17 04:21:44 +0100

    index.js fixed

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import mongoose, from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import Path from 'path';
-import userRouter from './routes/user.route.js';   
+import path from 'path';
 
 dotenv.config();
 
